190cc45c8e85af05771ab888be02d68e40f7885f
evosynchtech solutions <virtualmodify@gmail.com>
2024-06-06 21:38:31 +0530

    first commit after creation project

---
+++ home/admin.py
@@ -0,0 +1,89 @@
+from django import forms
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from .models import Carousel, Service, Blog, Testimonial, Feature, Career, ContactUS, Client, Enquiry, ApplyJob, JobType
+
+@admin.register(Carousel)
+class CarouselAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description',)
+    search_fields = ('title',)
+    list_filter = ('title',)
+
+    class CarouselForm(forms.ModelForm):
+        class Meta:
+            model = Carousel
+            fields = '__all__'
+
+        def clean_title(self):
+            title = self.cleaned_data.get('title')
+            words = title.split(" ")
+            if len(words) < 1:
+                raise ValidationError(
+                    _('Title should not contain only one word.'),
+                    code='invalid'
+                )
+            return title
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['title'].help_text = "Title should not contain only one word.Keep it two words for better UI"
+        return form 
+
+    form = CarouselForm
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'logo',)
+    search_fields = ('title',)
+    list_filter = ('title',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['logo'].help_text = "Copy-Paste Font Awesome Icon Code Snippet"
+        return form 
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at',)
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('company_name',)
+    search_fields = ('company_name',)
+
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_description',)
+    search_fields = ('title',)
+
+@admin.register(Career)
+class CareerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'position', 'location',)
+    search_fields = ('title', 'position',)
+    list_filter = ('location', 'job_type',)
+
+@admin.register(ContactUS)
+class ContactUSAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone_number',)
+    search_fields = ('first_name', 'last_name', 'email',)
+    list_filter = ('email',)
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('company_name',)
+    search_fields = ('company_name',)
+
+@admin.register(Enquiry)
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ('website', 'email',)
+    search_fields = ('email',)
+
+@admin.register(ApplyJob)
+class ApplyJobAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email',)
+    search_fields = ('first_name', 'last_name', 'email',)
+
+admin.site.register(JobType)
---
+++ home/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.homePage, name="home_page"),
+    path("about_us/", views.about_us, name="about_us"),
+    path("contact_us/", views.contact_us, name="contact_us"),
+    path("blogs/",views.blog_list,name="blog_list"),
+    path("blog/detail/<int:id>/",views.blog_detail,name="blog_detail"),
+    path("services/",views.service_list,name="service_list"),
+    path("service/detail/<int:id>/",views.service_detail,name="service_detail"),
+    path("careers/",views.career_list,name="career_list"),
+    path("career/detail/<int:id>/",views.career_detail,name="career_detail"),
+    path("enquiry/",views.enquiry,name="enquiry"),
+    path("contact/",views.contact_us_view,name="contact_us_view"),
+    path("apply_job/",views.apply_job,name="apply_job"),
+    path("privacy_policy/",views.privacy_policy,name="privacy_policy")
+]
